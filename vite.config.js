cc7e5f36142dba58f9b6703026103aa655148cf9
victorialewis2618 <victorialewis2618@gmail.com>
2026-01-09 12:19:40 +0530

    chore: force redeploy v2

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Force deploy trigger
+  // Force deploy trigger v2 - timestamp 2026-01-09
 })
